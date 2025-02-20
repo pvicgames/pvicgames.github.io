41bdb36d36e43329c0ac490be61352ca88ce2051
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-02-20 16:50:06 -0300

    Added background and logo

---
+++ src/components/VideoBackground.tsx
@@ -0,0 +1,18 @@
+//import React from 'react'
+
+const VideoBackground = () => {
+  return (
+    <video
+      className="fixed min-w-screen min-h-screen bottom-0 left-0 object-cover overflow-hidden
+                 grayscale opacity-8 blur-[2px]"
+      autoPlay
+      loop
+      muted
+    >
+      <source className="" src="videos/artius.mp4" />
+      Your browser does not support the video tag.
+    </video>
+  );
+};
+
+export default VideoBackground;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,23 @@
 //import React from 'react'
+import VideoBackground from "../components/VideoBackground";
+
+import pvicLogo from "../assets/img/pvic_logo_blue.png";
 
 const Home = () => {
   return (
     <div>
-      <h1>Eae</h1>
+      <VideoBackground />
+
+      <div className="relative xl:columns-2 sm:columns-1">
+        <div className="p-10 xl:min-h-screen flex justify-center items-center">
+          <img
+            className="max-h-[450px] object-contain animate__animated animate__rubberBand"
+            src={pvicLogo}
+          />
+        </div>
+
+        <div className="xl:min-h-screen"></div>
+      </div>
     </div>
   );
 };
