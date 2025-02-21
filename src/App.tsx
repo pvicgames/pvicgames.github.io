1dc8203e5ccfc95b243b079db8d543d1bdb5780f
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-02-21 20:25:27 -0300

    Started contact page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "animate.css";
 
 import { HashRouter as Router, Routes, Route } from "react-router";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,16 @@
+//import React from "react";
+import VideoBackground from "../components/VideoBackground";
+import PvicDots from "../components/PvicDots";
+import Navbar from "../components/Navbar";
+
+const Contact = () => {
+  return (
+    <div>
+      <VideoBackground />
+      <PvicDots />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Contact;
