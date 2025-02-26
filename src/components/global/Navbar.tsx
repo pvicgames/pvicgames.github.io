02e4b44f6c18d7c1a9893a02534fc004ac5ea3b1
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-02-26 15:26:17 -0300

    navbar translation

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,16 +1,19 @@
 //import React from "react";
+import { useTranslation } from "react-i18next";
 import NavbarButton from "./NavbarButton";
 
 const Navbar = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="fixed h-screen z-20 flex items-end pointer-events-none">
       <div className="sticky h-[50px] w-screen">
         <div className="absolute w-screen bottom-0 bg-pvic-pink h-[40px]" />
 
         <div className="relative pointer-events-auto flex items-end ">
-          <NavbarButton text="🏠 Home" link="/" />
-          <NavbarButton text="💭 About" link="/about" />
-          <NavbarButton text="📧 Contact" link="/contact" />
+          <NavbarButton text={t("navbar.home")} link="/" />
+          <NavbarButton text={t("navbar.about")} link="/about" />
+          <NavbarButton text={t("navbar.contact")} link="/contact" />
         </div>
       </div>
     </div>
