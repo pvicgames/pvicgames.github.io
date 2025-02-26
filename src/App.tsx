c7d6c2f14013a5cd54caed1e7385fc54141317dc
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-02-26 14:35:52 -0300

    translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import "./styles.css";
 import "animate.css";
 
+import i18n from "./i18n";
 import { HashRouter as Router, Routes, Route } from "react-router";
+import { useEffect } from "react";
 
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 
 function App() {
+  useEffect(() => {
+    i18n.changeLanguage(navigator.language)
+  }, [])
+
   return (
     <Router>
       <Routes>
---
+++ src/i18n.ts
@@ -0,0 +1,16 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./locales/en.json";
+import pt from "./locales/pt.json";
+
+i18n.use(initReactI18next).init({
+  debug: true,
+  fallbackLng: "en",
+
+  resources: {
+    en: { translation: en },
+    pt: { translation: pt }
+  }
+});
+
+export default i18n;
