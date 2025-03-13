803092de29997ff2f8539dd4ab41f2acf2b08ba5
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-03-13 18:13:06 -0300

    Added parallax and new artius page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./styles.css";
 import "animate.css";
 
 import i18n from "./i18n";
+
 import { HashRouter as Router, Routes, Route } from "react-router";
 import { useEffect } from "react";
 
@@ -9,23 +10,28 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import EdMescDX from "./pages/EdMescDX";
+import NewArtius from "./pages/NewArtius";
 import Artius from "./pages/Artius";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 function App() {
   useEffect(() => {
-    i18n.changeLanguage(navigator.language)
-  }, [])
+    i18n.changeLanguage(navigator.language);
+  }, []);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/"         element={<Home />} />
-        <Route path="/about"    element={<About />} />
-        <Route path="/contact"  element={<Contact />} />
-        <Route path="/artius"   element={<Artius />} />
-        <Route path="/edmescdx" element={<EdMescDX />} />
-      </Routes>
-    </Router>
+    <ParallaxProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/artius" element={<Artius />} />
+          <Route path="/newartius" element={<NewArtius />} />
+          <Route path="/edmescdx" element={<EdMescDX />} />
+        </Routes>
+      </Router>
+    </ParallaxProvider>
   );
 }
 
