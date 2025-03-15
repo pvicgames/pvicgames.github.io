c6960f4cc698e598c2326779c0aa2aa418d7329a
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-03-15 20:07:49 -0300

    added padding exclusive on desktops

--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -9,7 +9,7 @@ const SocialIcon = (props: Props) => {
   return (
     <a href={props.Link} target="_blank" className="z-10">
       <img className="w-[130px] rounded-2xl drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] duration-100
-                      ml-3 mr-3
+                      xl:ml-3 xl:mr-3
                       hover:drop-shadow-[0px_0px_10px_rgba(255,255,255,255)] hover:scale-115"
            src={props.Icon}/> 
     </a>
