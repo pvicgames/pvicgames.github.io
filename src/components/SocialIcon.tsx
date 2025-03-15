f06b79abc8e309dedf1925fe0654f7423fc02fe5
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-03-15 17:04:40 -0300

    fixed mouse bug

--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const SocialIcon = (props: Props) => {
   return (
-    <a href={props.Link} target="_blank">
+    <a href={props.Link} target="_blank" className="z-10">
       <img className="w-[130px] rounded-2xl drop-shadow-[0px_0px_5px_rgba(255,255,255,1)] duration-100
                       ml-3 mr-3
                       hover:drop-shadow-[0px_0px_10px_rgba(255,255,255,255)] hover:scale-115"
