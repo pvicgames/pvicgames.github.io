54195c8e63bf62ac9d578fb3a1aa69cc8a2260da
Gabriel Casanova <con.casanovaproductions@outlook.com>
2026-03-09 00:29:45 -0300

    Updated credits

--- src/pages/ArtiusCredits.tsx
+++ src/pages/ArtiusCredits.tsx
@@ -4,16 +4,19 @@ import ActualPage from '../components/global/ActualPage'
 const ArtiusCredits = () => {
   return (
     <ActualPage singleColumn={true} noNavbar={true}>
-      <div className='m-auto text-center text-[20px]'>
+      <div className='m-auto text-center text-[20px] max-w-[1000px]'>
         <div className="mt-20 m-auto">
           <p>Team Leader, Game Director, Lead Programmer and Stage Designer<br />
-            <strong>Pedro Victor Gonçalves</strong></p>
+            <strong>Pedro Victor 'PVic' Gonçalves</strong></p>
 
-          <p>Game Producer, Programmer, Narrative Designer and Additional Marketing<br />
+          <p>Game Producer, Programmer and Narrative Designer<br />
             <strong>Gabriel Casanova</strong></p>
 
           <p>Gameplay Programmer and Stage Designer<br />
             <strong>Kevin “PranchaD” Macedo</strong></p>
+            
+          <p>Level Design<br />
+            <strong>Bánhegyi “Balibali” Balázs</strong>, <strong>Lucei Bits</strong></p>
         </div>
 
         <div className="mt-20">
@@ -34,7 +37,7 @@ const ArtiusCredits = () => {
             <strong>SpeedVGM</strong></p>
 
           <p>Music Composer, Performer and Music Arranger<br />
-            <strong>SaGa!</strong></p>
+            <strong>SaGa!</strong>, <strong>Luan Maziero</strong>, <strong>ruby</strong></p>
         </div>
 
         <div className="mt-20">
@@ -46,10 +49,10 @@ const ArtiusCredits = () => {
             <strong>Fishibi</strong>, <strong>BITTO_art</strong></p>
 
           <p>Additional Assets<br />
-            <strong>Retr0Scum</strong>, <strong>Dayv</strong>, <strong>Espoir Souriant</strong></p>
+            <strong>Retr0Scum</strong>, <strong>Dayv Divver</strong>, <strong>Espoir Souriant</strong>, <strong>Phylolint</strong></p>
 
           <p>Additional Programmer<br />
-            <strong>Zazon</strong></p>
+            <strong>Zazon</strong>, <strong>Minami</strong></p>
 
           <p>Additional Music<br />
             <strong>Glace Sue</strong>, <strong>Renzo Battisti</strong>, <strong>Gabriel Miller</strong>, <strong>Skullduggery</strong>, <strong>Volcanolotus</strong></p>
@@ -69,26 +72,32 @@ const ArtiusCredits = () => {
             <li><strong>Harmony Framework</strong> ................................. Darkn</li>
             <li><strong>Orbinaut Framework</strong> ............................. Triangly</li>
             <li><strong>Batch Palette Scaler</strong> ......................... MrLevRocks</li>
+            <li><strong>REZOL</strong> .................................... Foxy of Jungle</li>
           </ul>
         </div>
 
         <div className="mt-20">
           <h2 className="text-4xl font-bold">Testers<br /><span className="text-base italic">In Alphabetical Order</span></h2>
-          <p><strong>Balibali</strong>, <strong>BriceTAS</strong>, <strong>Campers.arts</strong>, <strong>Defnotreal_</strong>, <strong>Ephed</strong>,<br />
-          <strong>Jackie!</strong>, <strong>JMan</strong>, <strong>Jordan</strong>, <strong>justWiz</strong>, <strong>SoniKast</strong>, <strong>Log</strong>, <strong>Mr. Freaks</strong>,<br />
-          <strong>Noah Copeland</strong>, <strong>Ommelete Studios</strong>, <strong>Pedrinho</strong>,<br />
-          <strong>Polar-Star</strong>, <strong>Retr0scum</strong>, <strong>Rosie</strong>, <strong>Seraphic_elf</strong>,<br />
-          <strong>Skullduggery</strong>, <strong>TheToastedGuy</strong>, <strong>Trakinas</strong>, <strong>Troopsushi</strong>,<br />
-          <strong>UsagiDood</strong>, <strong>VolcanoLotus</strong>, <strong>Yousucks001</strong></p>
+          <p>
+            <strong>Balibali</strong>, <strong>Berry</strong>, <strong>BriceTAS</strong>, <strong>Campers.arts</strong>, <strong>Defnotreal_</strong>, 
+            <strong>DigitalPOP</strong>, <strong>Ephed</strong>, <strong>Groundpork</strong>, <strong>Jackie!</strong>, <strong>JMan</strong>, 
+            <strong>Jordan</strong>, <strong>Jovis</strong>, <strong>justWiz</strong>, <strong>just_t0rii</strong>, <strong>Log</strong>, 
+            <strong>LUA</strong>, <strong>Luan Maziero</strong>, <strong>Mors</strong>, <strong>Mr. Freaks</strong>, <strong>MykeLegin</strong>, 
+            <strong>Noah Copeland</strong>, <strong>Ommelete Studios</strong>, <strong>Pedrinho</strong>, <strong>Polar-Star</strong>, 
+            <strong>Retr0scum</strong>, <strong>Riegel25</strong>, <strong>Rosie</strong>, <strong>Samwow</strong>, <strong>Seraphic_elf</strong>, 
+            <strong>Skullduggery</strong>, <strong>SoniKast</strong>, <strong>Spectern</strong>, <strong>TheToastedGuy</strong>, 
+            <strong>Trakinas_web</strong>, <strong>Troopsushi</strong>, <strong>UsagiDood</strong>, <strong>VolcanoLotus</strong>, <strong>Yousucks001</strong>
+          </p>
         </div>
 
         <div className="mt-20">
           <h2 className="text-4xl font-bold">Previous Patreon Supporters<br /><span className="text-base italic">In Alphabetical Order</span></h2>
-          <p><strong>Adon Adaniel</strong>, <strong>Captan</strong>, <strong>Defnotreal_</strong>,<br />
-          <strong>Groundpork</strong>, <strong>JinoDaPuppet</strong>, <strong>McVickroll</strong>,<br />
-          <strong>NewTimothy28</strong>, <strong>Ommelete Studios</strong>,<br />
-          <strong>Retr0scum</strong>, <strong>Shane B</strong>, <strong>Skullduggery</strong>,<br />
-          <strong>Yousucks001</strong></p>
+          <p>
+            <strong>Adon Adaniel</strong>, <strong>Captan</strong>, <strong>Defnotreal_</strong>, 
+            <strong>Groundpork</strong>, <strong>JinoDaPuppet</strong>, <strong>McVickroll</strong>, 
+            <strong>NewTimothy28</strong>, <strong>Ommelete Studios</strong>, <strong>Retr0scum</strong>, 
+            <strong>Shane B</strong>, <strong>Skullduggery183</strong>, <strong>Yousucks001</strong>
+          </p>
         </div>
 
         <div className="mt-20">
@@ -98,6 +107,7 @@ const ArtiusCredits = () => {
             <li><strong>Retr0scum</strong>, for the character designs of Tyler and Mrs. Dodo</li>
             <li><strong>Adam Starcaster</strong>, with gratitude for his invaluable support</li>
             <li><strong>Marco Frisina</strong>, for opening doors along the way</li>
+            <li><strong>Berry</strong>, for the online server</li>
             <li><strong>Lucia Ippolito</strong>, for her unwavering encouragement</li>
             <li><strong>Anabel & Fabi</strong>, the musketeers</li>
             <li><strong>Miller, Gregório, Renzo, Victor & Vitor</strong>, for the inspirations and the memories</li>
